86ce1e442ea33f1ee22551fcb8f07fce05610b10
Kevin Longeway <KevinLongeway@gmail.com>
2025-11-18 19:46:57 -0700

    Added rotate to the player ship

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     print("Starting Asteroids")
@@ -13,18 +14,21 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
 
-    while True:
-        log_state()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
+    running = True
+    while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
+                running = False
 
-        screen.fill((0,0,0))  #Fill background
-        pygame.display.flip()   #Update display
-        dt = clock.tick(60)/1000
+        screen.fill((0,0,0))  # Fill background
+        player.draw(screen)   # Draw player
+        log_state()   # write a frame's state
+        pygame.display.flip()   # Update display
+        dt = clock.tick(60) / 1000
+        player.update(dt)
 
-    log_state()   # no args, second write
     pygame.quit()
 
 if __name__ == "__main__":
